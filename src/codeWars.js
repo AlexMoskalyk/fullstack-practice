34dafa243a2e025911eb4f27d777e69d603163da
Oleksandr Moskalyk <alex.moskalik@gmail.com>
2024-09-10 13:30:34 +0200

    codewars tasks

--- src/codeWars.js
+++ src/codeWars.js
@@ -47,3 +47,25 @@ function between(a, b) {
   }
   return result;
 }
+//==========================================
+// 10.09.24
+
+// Given an array of integers, find the one that appears an odd number of times.
+
+// There will always be only one integer that appears an odd number of times.
+
+// Examples
+// [7] should return 7, because it occurs 1 time (which is odd).
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+function findOdd(A) {
+  const counts = {};
+  A.forEach(item => {
+    counts[item] = (counts[item] || 0) + 1;
+  });
+
+  return +Object.keys(counts).find(key => counts[key] % 2 !== 0);
+}
